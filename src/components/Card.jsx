df35f33699c92ed20b086413586260adf2308396
khorlo333 <24lp3394@gmail.com>
2024-12-19 22:15:31 +0800

    card added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,28 @@
+import "./card.css";
+
 export const NewsItems = [
   {
     imageUrl:
-      "https://dash-api.yld.mn/api/image/90d5d4f6-1b7e-464d-9247-1c137ee7365a",
+      "https://dash-api.yld.mn/api/image/cfd29a6b-73f6-4daf-ad3a-5c431949aedc",
+    newsType: "Улс төр",
+    newsTitle: "Утааны асуудлыг 'Яндангүй бүс' гаргаж шийдэх юм гэнэ",
+    newsAuthor: "С.Ангирмаа 12 сарын 19, 2024",
+  },
+
+  {
+    imageUrl:
+      "https://dash-api.yld.mn/api/image/f4cee43c-b1e6-4211-885b-8dc224a43114",
+    newsType: "Зах зээл",
+    newsTitle: "Ирэх онд нээлтээ хийх ТОП 10 кино",
+    newsAuthor: "М.Хулан 12 сарын 19, 2024",
+  },
+  {
+    // imageUrl:
+    //   "https://dash-api.yld.mn/api/image/90d5d4f6-1b7e-464d-9247-1c137ee7365a",
     newsType: "Бизнес",
     newsTitle: "Монложистикс групп экспортын тээвэрлэлтээ нэмэгдүүлнэ",
     newsAuthor: "С.Ангирмаа 12 сарын 18, 2024",
   },
-  // {
-  //   imageUrl: ,
-  //   newsType: ,
-  //   newsTitle: ,
-  //   newsAuthor
-
-  // },
-  // {
-  //   imageUrl: ,
-  //   newsType: ,
-  //   newsTitle: ,
-  //   newsAuthor
-
-  // },
   // {
   //   imageUrl: ,
   //   newsType: ,
@@ -31,11 +34,34 @@ export const NewsItems = [
 
 function Card(props) {
   return (
-    <div className="card">
-      <img src={props.image} alt="" />
-      <h3>{props.type}</h3>
-      <p>{props.title}</p>
-      <h4>{props.author}</h4>
+    <div className="container">
+      <div className="card">
+        <img src={props.image} alt="" />
+        <h3>{props.type}</h3>
+        <p>{props.title}</p>
+        <h4>{props.author}</h4>
+      </div>
+
+      <div className="sidePanel">
+        <div>
+          <img src={props.image} alt="" />
+          <h3>{props.type}</h3>
+          <p>{props.title}</p>
+          <h4>{props.author}</h4>
+        </div>
+        <div>
+          <h3>{props.type}</h3>
+          <p>{props.title}</p>
+          <h4>{props.author}</h4>
+        </div>
+      </div>
+
+      {/* <div>
+        <img src={props.image} alt="" />
+        <h3>{props.type}</h3>
+        <p>{props.title}</p>
+        <h4>{props.author}</h4>
+      </div> */}
     </div>
   );
 }
